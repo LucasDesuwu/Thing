fe001af583454e1d31307b07f74362a1f7bb9387
LucasDesuwu <Thedogebr@gmail.com>
2022-01-03 22:51:22 -0300

    Update IMC_2.0.py

--- IMC_2.0.py
+++ IMC_2.0.py
@@ -6,40 +6,35 @@ def main():
     peso = input ("Peso:")
     altcm = (float(altura)) / 100
     imc = (float(peso)) / (((float(altcm)) * ((float(altcm)))))
+    clear = lambda: os.system('cls')
 
     if imc < 18.5:
         input ("Seu IMC é " + str(imc)[:4] + " e você está abaixo do peso")
-        clear = lambda: os.system('cls')
         clear()
         main()
 
     if imc <= 24.9:
         input ("Seu IMC é " + str(imc)[:4] + " e você está no peso normal")
-        clear = lambda: os.system('cls')
         clear()
         main() 
 
     if imc <=29.9:
         input ("Seu IMC é " + str(imc)[:4] + " e você está sobrepeso")
-        clear = lambda: os.system('cls')
         clear()
         main() 
 
     if imc <= 34.9:
         input ("Seu IMC é " + str(imc)[:4] + " e você está com obesidade grau 1")
-        clear = lambda: os.system('cls')
         clear()
         main() 
 
     if imc <= 39.9:
         input ("Seu IMC é " + str(imc)[:4] + " e você está com obesidade grau 2")
-        clear = lambda: os.system('cls')
         clear()
         main()
 
     if imc > 40.0 :
         input ("Seu IMC é " + str(imc)[:4] + " e você está com obesidade grau 3")
-        clear = lambda: os.system('cls')
         clear()
         main()
 
